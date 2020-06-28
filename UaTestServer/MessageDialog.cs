c02f84039056a3cebc9e9c995c8d84878f3c8b8f
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2020-06-28 17:32:13 +0200

    Add message dialog

---
+++ UaTestServer/MessageDialog.cs
@@ -0,0 +1,39 @@
+﻿using Opc.Ua.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace UaTestServer
+{
+    internal class MessageDialog : IApplicationMessageDlg
+    {
+        private string _text;
+        private bool _ask;
+        public override void Message(string text, bool ask = false)
+        {
+            _text = text;
+            _ask = ask;
+        }
+
+        public override Task<bool> ShowAsync()
+        {
+            Console.WriteLine("{0}", _text);
+            return Task.FromResult(!_ask || Prompt());
+        }
+
+        private static bool Prompt()
+        {
+            ConsoleKey response;
+            do
+            {
+                Console.Write($"[y/n]");
+                response = Console.ReadKey(false).Key;
+                if (response != ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                }
+            } while (response != ConsoleKey.Y && response != ConsoleKey.N);
+
+            return (response == ConsoleKey.Y);
+        }
+    }
+}
